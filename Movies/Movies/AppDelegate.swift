7cd6af8bf597a0f8bda2eb8cefdf9135390adf27
Lorrayne Paraiso Carneiro Flor <lorrayneparaisoc@gmail.com>
2018-10-30 01:50:46 -0300

    Create base scenes and add custom tabb bar

--- Movies/Movies/AppDelegate.swift
+++ Movies/Movies/AppDelegate.swift
@@ -17,6 +17,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        if let tabBarController = self.window!.rootViewController as? UITabBarController {
+            tabBarController.selectedIndex = 1
+        }
+
         return true
     }
 
